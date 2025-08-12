9e13628e6049a6073da398e6b5027f5409f0af45
KRONTAGE <sukronmuhammad207@gmail.com>
2025-08-12 16:53:50 +0700

    update web

--- Dom.js
+++ Dom.js
@@ -1,22 +1,28 @@
 function toggle() {
-    document.querySelector(".dropleft").classList.toggle("show")
-    document.querySelector(".nav-box").classList.toggle("transform")
+  document.querySelector(".dropleft").classList.toggle("show")
+  document.querySelector(".nav-box").classList.toggle("transform")
 }
 
 const container = document.getElementById('scrollContainer');
-    const items = document.querySelectorAll('.box-content');
+const items = document.querySelectorAll('.box-content');
+const btns = document.querySelectorAll('.btn')
 
-    items.forEach(item => {
-      item.addEventListener('click', () => {
+items.forEach(item => {
+  item.addEventListener('click', () => {
 
-        items.forEach(i => i.classList.remove('active'));
+    btns.forEach(i => i.classList.remove('show'));
 
-        item.classList.add('active');
-        
-        const itemCenter = item.offsetLeft - (container.clientWidth / 2) + (item.clientWidth / 2);
-        container.scrollTo({
-          left: itemCenter,
-          behavior: 'smooth'
-        });
-      });
+    const btnInItem = item.querySelector('.btn');
+    if (btnInItem) btnInItem.classList.add('show');
+    
+    items.forEach(i => i.classList.remove('active'));
+
+    item.classList.add('active');
+
+    const itemCenter = item.offsetLeft - (container.clientWidth / 2) + (item.clientWidth / 2);
+    container.scrollTo({
+      left: itemCenter,
+      behavior: 'smooth'
     });
+  });
+});
